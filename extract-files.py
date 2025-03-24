c35ca083ba5b974fa3301e5600e8dedc35cbf089
Andrew Hexen <SyberHexen@gmail.com>
2025-03-24 00:17:10 -0300

    msm8998-common: Build SYSTEM_EXT and compress with squashfs
    * Going to be flashed to oem partition.
    
    Change-Id: I2f46c1d39b7453d9d384839fc23da44d274ec32a
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Co-authored-by: Deivid21 <david.parra.ignacio@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -64,13 +64,14 @@ blob_fixups: blob_fixups_user_type = {
         'system_ext/etc/permissions/com.qualcomm.qti.imscmservice-V2.0-java.xml',
         'system_ext/etc/permissions/com.qualcomm.qti.imscmservice-V2.1-java.xml',
         'system_ext/etc/permissions/com.qualcomm.qti.imscmservice-V2.2-java.xml',
-        'system_ext/etc/permissions/qcrilhook.xml',
         'system_ext/etc/permissions/telephonyservice.xml',
         'system_ext/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml',
         'system_ext/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml',
     ): blob_fixup()
-        .regex_replace('product', 'system_ext')
+        .regex_replace('system/product', 'system_ext')
         .regex_replace('xml version="2.0"', 'xml version="1.0"'),
+    'system_ext/etc/permissions/qcrilhook.xml': blob_fixup()
+        .regex_replace('product', 'system_ext'),
     (
         'system_ext/lib/lib-imscamera.so',
         'system_ext/lib/lib-imsvideocodec.so',
