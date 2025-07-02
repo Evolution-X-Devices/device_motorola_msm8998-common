e2196a442b27c2c02b3ccacf84e81a66cf3c898c
Deivid21 <david.parra.ignacio@gmail.com>
2025-07-02 02:04:17 -0400

    msm8998-common: overlay: Build TurboPowerOverlays from hardware/motorola
    Change-Id: I1a47611aebb0c2b01c66b9476d5934dc26eb66ad

--- common.mk
+++ common.mk
@@ -191,7 +191,8 @@ PRODUCT_COPY_FILES += \
 # MotoActions
 PRODUCT_PACKAGES += \
     MotoActions \
-    MotoCommonOverlay
+    MotoCommonOverlay \
+    TurboPowerOverlay
 
 # OMX
 PRODUCT_PACKAGES += \
